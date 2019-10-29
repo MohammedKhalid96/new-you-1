3ad74c773bb00204302a2c4e93db195e0557a11a
Mohammed Khalid <mohammedkhalid96@outlook.com>
2019-10-29 15:18:01 +0200

    ajsnASsaaa

--- js/main.js
+++ js/main.js
@@ -57,12 +57,12 @@ $(document).ready(function() {
     }
   });
 
-  var rellax = new Rellax(".rellax", {
-    speed: -2,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
+//   var rellax = new Rellax(".rellax", {
+//     speed: -2,
+//     center: false,
+//     wrapper: null,
+//     round: true,
+//     vertical: true,
+//     horizontal: false
+//   });
 });
